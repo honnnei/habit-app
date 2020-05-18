ac90889b35bf1b659d1a5ac5a838c6054b231b7e
jamesabustan <jamesabustan04@gmail.com>
2020-05-18 15:32:34 +0100

    update files

---
+++ client/src/test/App.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
+import { shallow, mount } from 'enzyme';
+
+describe('App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+  // it('matches the snapshot', () => {
+  //   const tree = renderer.create(<App />).toJSON();
+  //   expect(tree).toMatchSnapshot();
+  // });
+  // test('renders Navbar', () => {
+  //   expect(wrapper.containsMatchingElement(<Navbar />)).toEqual(true);
+  // });
+  test('contains Routes', () => {
+    expect(wrapper.containsMatchingElement(<Route />)).toEqual(true);
+  });
+  test('contains 4 Routes', () => {
+    expect((wrapper.find('Route').length)).toEqual(4);
+  });
+   it('Route to HomePage is exact', () => {     
+    let wrap = mount(<App />);                                 
+    let route = wrap.find(<Route path='/' exact/>);
+    expect(route).toBeTruthy();
+   });
+   it('Route to NewUser is exact', () => {   
+    let wrap = mount(<App />);                                       
+    let route = wrap.find( <Route path='/newUser' exact/>);
+    expect(route).toBeTruthy();
+   });
+   it('Route to Tracker is exact', () => {   
+    let wrap = mount(<App />);                                       
+    let route = wrap.find( <Route path='/tracker' exact/>);
+    expect(route).toBeTruthy();
+   });
+   it('Route to Habit Tracker Add is exact', () => {   
+      let wrap = mount(<App />);                                       
+      let route = wrap.find( <Route path='/habit/add' exact/>);
+      expect(route).toBeTruthy();
+     });
+});
+

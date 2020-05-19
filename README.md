d5a2903fb0682420391180efc02521221a046d0e
honnnei <hannpop@gmail.com>
2020-05-19 18:43:12 +0200

    in the process of introducing React Hooks

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # habit-app
 ### James Hanna Steven Huseyin
 
+### Project Name :  (Habit Tracker)
+Hanna, Huseyin, Steven, James
+
+### Project Brief / User Stories
+
+In the project directory, you can run:
+
+### To start (instructions on how to initialise App)
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br />
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
 ## Installation
 
 ## About
@@ -11,7 +31,19 @@
 * <Router /> tag can only be use once in the App, otherwise the links and routes don't render components correctly
 - in tests with enzyme and jest:
 * <BrowserRouter> tag needs to wrap the component in wrapper to avoid the error 'Link needs to be in Router'
-* inconsistency in tests working and not find.lenght etc.
+* inconsistency in tests working and not find.lenght etc - > *TO FIX*
+* BUG -> when you create a new user and go straight into creating habits and create a habit and press 'ADD' -> it redirects you to Tracker but doesn't map the array of habit for some reason. After you go through the 'login' route it does -> *TO FIX*
+* passing a parameter AND event in handleChange: solved:
+
+defining the function:
+```
+handleChange = (event, habitIndex) => {}
+```
+in input:
+```
+onChange={event => this.handleChange(event, index)}
+```
+* A component is changing an uncontrolled input of type number to be controlled.
 
 ## How we would develop our app further
 
@@ -28,7 +60,8 @@ Hanna:
 * tested frontend
 
 James:
-* 
+* created basic frontend
+* tested frontend
 
 Plan for today:
 - create axios requests
--- client/src/components/Habit.js
+++ client/src/components/Habit.js
@@ -1,63 +1,94 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from "axios"
-
-class Habit extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = ({
-      nameOfUser: props.location.username,
-      habitName: "",
-      frequency: 0,
-      tracking: [false]
-
-      /* action: '' */
-    })
-  }
-
-  createHabit = () => {
-    console.log(this.state.nameOfUser);
-    if (this.state.nameOfUser) {
-      axios.put(`habits/add-habit/${this.state.nameOfUser}`, {
-        habitName: this.state.habitName,
-        frequency: this.state.frequency,
-        date: new Date().toISOString().split('T')[0],
-        tracking: this.state.tracking
-      })
-      .then(response => console.log(response))
-      .catch(error => {
-        console.log("this is error", error.message);
-      });
-    }
-  }
-
-  handleChange = (event) => {
-    event.preventDefault();
-    const {name, value} = event.target
-    this.setState({ [name]: value })
-  }
+import { useState, useEffect } from 'react';
 
 
-  render() {
+// class Habit extends React.Component {
+  const Habit = () => {
+  const [ Frequency, setFrequency ] = useState("");
+  const onChange = e => {
+    e.persist();
+    setFrequency(e.target.value);
+  };
+  useEffect(() => {
+    console.log("Search message inside useEffect: ", Frequency);
+  }, [Frequency]);
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = ({
+  //     nameOfUser: props.location.username,
+  //     habitName: "",
+  //     frequency: undefined,
+  //     trackingArray: []
+
+  //     /* action: '' */
+  //   })
+  // }
+
+  // createHabit = () => {
+  //   console.log(this.state.nameOfUser);
+  //   if (this.state.nameOfUser) {
+  //     axios.put(`habits/add-habit/${this.state.nameOfUser}`, {
+  //       habitName: this.state.habitName,
+  //       frequency: this.state.frequency,
+  //       date: new Date().toISOString().split('T')[0],
+  //       tracking: this.state.trackingArray
+  //     })
+  //     .then(response => console.log(response))
+  //     .catch(error => {
+  //       console.log("this is error", error.message);
+  //     });
+  //   }
+  // }
+
+  // updateTrackingByFrequency = () => {
+  //   console.log(`handle change 4 ${this.state.frequency}`)
+  //   console.log('this is update trackin');
+  //   let array = []
+  //   for (let i=0; i < this.state.frequency; i++) {
+  //     array.push('we pushed it')
+  //   }
+  //   console.log(array);
+  //   this.setState({
+  //     trackingArray: array
+  //   })
+  // }
+
+  // handleChange = (event) => {
+  //   console.log(`handle change 1 ${this.state.frequency}`)
+  //   event.preventDefault();
+  //   const {name, value} = event.target
+  //   this.setState({ [name]: value })
+  //   console.log(`handle change 2 ${this.state.frequency}`)
+  //   if (this.state.frequency) {
+  //     console.log(`handle change 3 ${this.state.frequency}`)
+  //     this.updateTrackingByFrequency()
+  //   }
+  // }
+
+
+  // render() {
       return (
         <div className="habit-container">
             <form>
                 <h3>Create Habit</h3>
                 <label htmlFor="habitName">What habit would you like to track?</label>
-                    <input
+                    {/* <input
                     id="habitName" 
                     name="habitName" 
                     type="text"
                     value={this.state.habitName}
                     onChange={this.handleChange}
-                    ></input>
+                    ></input> */}
                 <label htmlFor="frequency">How many times per day are you looking to do this?</label>
                 <input
-                    id="frequency" 
-                    name="frequency" 
+                    id="rrequency" 
+                    name="Frequency" 
                     type="number"
-                    value={this.state.frequency}
-                    onChange={this.handleChange}
+                    value={Frequency}
+                    onChange={onChange}
                     ></input>
                   {/* <label>Action</label>
                     <input 
@@ -68,11 +99,11 @@ class Habit extends React.Component {
                     ></input> */}
             </form>
             <div>
-              <Link to={{pathname:'/tracker', username: this.state.nameOfUser}}><button onClick={this.createHabit}>Add</button></Link>
+              {/* <Link to={{pathname:'/tracker', username: this.state.nameOfUser}}><button onClick={this.createHabit}>Add</button></Link> */}
             </div>
           </div>  
       );
-  }
+  // }
 }
 
 export default Habit;

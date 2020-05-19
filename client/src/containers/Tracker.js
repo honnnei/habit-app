d25f0ba8a912f89a52d9ea805c91c1674a3b8082
honnnei <hannpop@gmail.com>
2020-05-19 13:28:15 +0200

    habit array displays in Tracke.js

--- client/src/containers/Tracker.js
+++ client/src/containers/Tracker.js
@@ -6,19 +6,21 @@ class Tracker extends React.Component {
   constructor(props) {
     super(props);
     this.state = ({
-      username: props.location.username,
-      userData: null
+      userName: props.location.username,
+      userData: {}
     })
   }
 
   getUserHabits = () => {
-    axios(`/habits/${this.state.username}`)
+    axios(`/habits/${this.state.userName}`)
     .then(response => response.data)
     .then(array => {
       this.setState({
             userData: array,
             loading:false
           });
+      console.log(this.state.userData)
+      console.log(this.state.userData.habit)
     })
   }
 
@@ -28,13 +30,20 @@ class Tracker extends React.Component {
 
   
   render() {
+
+    let habitArray = []
+    if (this.state.userData.habit) {
+      habitArray = this.state.userData.habit.map(habit => (
+        <h1>{habit.habitName}</h1> 
+      )) 
+    }
+    // {habit.tracking[habit.tracking.length - 1] ? 'Done' : 'Not Done' 
+    console.log(this.state.userData.habit)
       return(
         <div className="trackerDiv">
-            <h1>{this.state.username}</h1>
-            {/* {this.state.userData.habit.map(habit => (
-              <h1>{habit.name} is {habit.tracking[habit.tracking.length - 1] ? 'Done' : 'Not Done' }</h1> 
-            ))} */}
-            <Link to='/habit/add'>Create Habit</Link>
+            <h1>{this.state.userName}</h1>
+           {habitArray}
+            <Link to={{pathname:'/habit/add', username:this.state.userName}}>Create Habit</Link>
         </div>
       );
   }

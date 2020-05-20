1cb29f95b5cb78ff7cb8cf6e80c19737249f89a4
jamesabustan <jamesabustan04@gmail.com>
2020-05-20 16:43:39 +0100

    refactor - app working

--- client/src/containers/Tracker.js
+++ client/src/containers/Tracker.js
@@ -1,11 +1,6 @@
-import React, {
-  useState,
-  useEffect
-} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {
-  Link
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 

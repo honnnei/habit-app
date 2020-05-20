000a07621b0a18ee05528a9f16e1e8fe20a4cfe1
jamesabustan <jamesabustan04@gmail.com>
2020-05-20 17:23:43 +0100

    modal feature integration

--- client/src/containers/Tracker.js
+++ client/src/containers/Tracker.js
@@ -2,16 +2,15 @@ import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import ProgressBar from 'react-bootstrap/ProgressBar';
-
-// import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 
 
 // const ModalExample = (props) => {
-//   const {
-//     buttonLabel,
-//     className
-//   } = props;
+//   // const {
+//   //   buttonLabel,
+//   //   className
+//   // } = props;
 
 //   const [modal, setModal] = useState(false);
 
@@ -41,6 +40,58 @@ const Tracker = (props) => {
   const [userName, setUserName] = useState("");
   const [userData, setUserData] = useState();
   const [progress, setProgress] = useState(0);
+  const [ habitFrequency, setHabitFrequency ] = useState(0);
+  const [ habitName, setHabitName ] = useState("");
+  const [ trackingArray, setTrackingArray ] = useState([]);
+
+      const handleName = e => {
+        e.persist();
+        setHabitName(e.target.value);
+      };
+
+      const handleFrequency = e => {
+        e.persist();
+        setHabitFrequency(e.target.value);
+      let array = [];
+        for ( let i = 0; i < e.target.value; i++ ) {
+          array.push(false);
+        }
+        setTrackingArray(array);
+      }
+
+      const createHabit = () => {
+        if (userName) {
+          axios.put(`habits/add-habit/${userName}`, {
+            habitName: habitName,
+            frequency: habitFrequency,
+            date: new Date().toISOString().split('T')[0],
+            tracking: trackingArray
+          })
+          .then(response => response)
+          .catch(error => {
+            console.log("this is error", error.message);
+          });
+          getUserHabits();
+          toggle();
+        }
+      }
+
+      useEffect(() => {
+        console.log("this is habit freaquency: ", habitFrequency);
+      }, [habitFrequency]);
+
+      useEffect(() => {
+        console.log("this is habit name: ", habitName);
+      }, [habitName]);
+
+  // const {
+  //   buttonLabel,
+  //   className
+  // } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
 
   const getUserHabits = () => {
     axios(`/habits/${userName}`)
@@ -84,6 +135,7 @@ const Tracker = (props) => {
       return dailyProgressPercentage;
     }
   }
+
 
 
   const mapHabitArray = () => {
@@ -132,9 +184,42 @@ const Tracker = (props) => {
         <ProgressBar now={progressBar()} label = {progressBar()} variant = "success" />
         <h1> {progress}</h1> 
         <h1> {userName}</h1>
-        <Link to={{pathname: '/habit/add', username: userName}}>Create Habit</Link> 
+        <div className="habit-modal">
+          <Button color="danger" onClick={toggle}>+</Button>
+          <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+            <ModalBody>
+            <div className="habit-container">
+                <form>
+                    <h3>Create Habit</h3>
+                    <label htmlFor="habitName">What habit would you like to track?</label>
+                        <input
+                        id="habitName" 
+                        name="habitName" 
+                        type="text"
+                        value={habitName}
+                        onChange={handleName}
+                        ></input>
+                    <label htmlFor="frequency">How many times per day are you looking to do this?</label>
+                    <input
+                        id="rrequency" 
+                        name="habitFrequency" 
+                        min="1"
+                        max="10"
+                        type="number"
+                        value={habitFrequency}
+                        onChange={handleFrequency}
+                        ></input>
+                  </form>
+                </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={() => {toggle(); createHabit();}}>Create</Button>
+              <Button color="secondary" onClick={toggle}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+        </div>
         {mapHabitArray()}
-        <ModalExample />
       </div>
       :
         <div>

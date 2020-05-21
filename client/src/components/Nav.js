a2f9fcf7eb73b6791a390de0cca2b93b0d0c8b68
jamesabustan <jamesabustan04@gmail.com>
2020-05-21 20:15:39 +0100

    logo added on navbar, page adjuste to fit full width

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Logo from '../img/marble.png';
 
 export class Nav extends Component {
     render() {
         return (
             <div>
-                <nav class="nav navHeader">
-                    <h3>Marbles - Habit Tracker</h3>
+                <nav class="nav navHeader navbarHome">
+                    <h3><img className="logoHead" src={Logo} alt="logo"/> Marbles - Habit Tracker</h3>
                 </nav>
             </div>
         )
